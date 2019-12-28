811a45ae7a089e773faea7788419748b0ba2a65e
juju4 <juju4@users.noreply.github.com>
2019-12-28 10:35:43 -0500

    test/default: log content serverspec check

--- test/integration/default/serverspec/syslogclient_spec.rb
+++ test/integration/default/serverspec/syslogclient_spec.rb
@@ -24,6 +24,20 @@ describe file('/etc/rsyslog.d/10-remotesyslog.conf') do
   its(:content) { should match /\*.\*     @10.1.1.1/ }
 end
 
+describe file('/var/log/syslog'), :if => os[:family] == 'ubuntu' do
+  it { should be_file }
+  its(:content) { should_not match /open error: Permission denied/ }
+  its(:content) { should_not match /syslogd: action 'action .*' resumed/ }
+  its(:content) { should_not match /syslogd: action 'action .*' suspended/ }
+end
+
+describe file('/var/log/messages'), :if => os[:family] == 'redhat' do
+  it { should be_file }
+  its(:content) { should_not match /open error: Permission denied/ }
+  its(:content) { should_not match /syslogd: action 'action .*' resumed/ }
+  its(:content) { should_not match /syslogd: action 'action .*' suspended/ }
+end
+
 ## with server set
 #logger TroubleshootingTest
 #echo test | logger -t mytest -d -n 10.1.1.1
